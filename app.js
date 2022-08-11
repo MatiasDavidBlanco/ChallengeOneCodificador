f5da48e9797d7ec898848e7c08f5d1257f54e537
Matias Blanco <100545487+MatiasDavidBlanco@users.noreply.github.com>
2022-08-10 22:28:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+var texto="";
+var textoCifrado="";
+var textoDescifrado="";
+
+function stop(event){
+    event.preventDefault()
+};
+
+function encriptar (){
+    texto= document.getElementById("text-input").value
+    if (texto==""){
+        alert("Ingrese un texto para encriptar")
+        stop();
+       
+    }
+    if (texto.search(/[A-Z]/g) > -1 || texto.search(/([áéíóúü])+/g) > -1 || texto.search(/[^A-z\s\d][\\\^]?/g) > -1) {
+        alert("No se aceptan letras en minuscula, acentos ni carácteres especiales");
+    }else{
+        textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+        document.getElementById("text-outputOn").innerHTML = textoCifrado;
+        document.getElementById("text-outputOff").style.display= "none"
+        document.getElementById("copiar-btn").style.display= "block"
+        document.getElementById("text-input").value= "";
+    }
+}
+
+
+function desencriptar(){
+ 
+    textoDescifrado = textoCifrado.replace(/(enter)/gi,"e").replace(/(imes)/gi,"i").replace(/(ai)/gi,"a").replace(/(ober)/gi,"o").replace(/(ufat)/gi,"u");
+    document.getElementById("text-outputOn").innerHTML = textoDescifrado;
+    document.getElementById("copiar-btn").style.display= "block"
+    
+  
+}
+function copiarAlPortapapeles() {
+        navigator.clipboard
+            .writeText(document.getElementById('text-outputOn').innerText)
+            .then(
+                success => alert("COPIADO!"), err => alert("error copying text")
+            );
+  }
+
+
+// FALTA:
+
+
+
+// Requisitos:
+// - Debe funcionar solo con letras minúsculas
+// - No deben ser utilizados letras con acentos ni caracteres especiales
+// - Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+
+// Por ejemplo:
+// "gato" => "gaitober"
+// gaitober" => "gato"
