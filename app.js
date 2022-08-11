17f285fbe4c0886872d6bd7fd5db6620334951d1
Matias Blanco <100545487+MatiasDavidBlanco@users.noreply.github.com>
2022-08-11 11:27:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -42,15 +42,3 @@ function copiarAlPortapapeles() {
   }
 
 
-// FALTA:
-
-
-
-// Requisitos:
-// - Debe funcionar solo con letras minúsculas
-// - No deben ser utilizados letras con acentos ni caracteres especiales
-// - Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
-
-// Por ejemplo:
-// "gato" => "gaitober"
-// gaitober" => "gato"
